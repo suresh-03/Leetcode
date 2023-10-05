0f5b5fb13822e98eee54c8e697741b25dc25d452
Suresh-03 <sureshkumar732003@gmail.com>
2023-10-05 06:55:42 +0530

    229

---
+++ majorityElementII_229.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class majorityElementII_229 {
+    public List<Integer> majorityElement(int[] nums) {
+        ArrayList<Integer> list = new ArrayList<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            // if(map.get(nums[i]) != null){
+            //     map.put(nums[i],1 + map.get(nums[i]));
+            // }
+            // else{
+            //     map.put(nums[i],0);
+            // }
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+
+        for(Map.Entry<Integer,Integer> set : map.entrySet()){
+            if(set.getValue() > nums.length / 3){
+                list.add(set.getKey());
+            }
+        }
+
+        System.out.println(map);
+
+
+        return list;
+    }
+}
