84d14cc6f7c9874778ce728928a339b08e781659
Suresh-03 <sureshkumar732003@gmail.com>
2023-10-23 10:07:55 +0530

    145

---
+++ _145_binaryTreePostorderTraverse.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class _145_binaryTreePostorderTraverse {
+     // Definition for a binary tree node.
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+       List<Integer> value = new ArrayList<>();
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if(root != null){
+            postorderTraversal(root.left);
+            postorderTraversal(root.right);
+            value.add(root.val);
+        }
+        return value;
+    }
+}
---
+++ _342_powerOfFour.java
@@ -0,0 +1,13 @@
+public class _342_powerOfFour {
+    class Solution {
+        public boolean isPowerOfFour(int n) {
+            if(n == 4 || n == 1){
+                return true;
+            }
+            if(n < 4 || n % 4 != 0){
+                return false;
+            }
+            return isPowerOfFour(n/4);
+        }
+    }
+}
