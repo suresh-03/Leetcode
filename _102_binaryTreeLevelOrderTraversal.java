628699824e39891488cc5f5f8a360f8e11d76fbf
Suresh-03 <sureshkumar732003@gmail.com>
2023-10-27 20:40:03 +0530

    102

---
+++ _102_binaryTreeLevelOrderTraversal.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+
+
+public class _102_binaryTreeLevelOrderTraversal {
+ 
+ // Definition for a binary tree node.
+  public class TreeNode {
+      int val;
+      TreeNode left;
+     TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if(root == null) return ans;
+        q.offer(root);
+        
+        while(!q.isEmpty()){
+            List<Integer> inner = new ArrayList<>();
+            int level = q.size();
+            for(int i = 0; i < level; i++){
+                if(q.peek().left != null) q.offer(q.peek().left);
+                if(q.peek().right != null) q.offer(q.peek().right);
+                inner.add(q.poll().val);
+            }
+            ans.add(inner);
+        }
+
+        return ans;
+        
+    }
+}
+
