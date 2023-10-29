4a20f1ed55bd2a049af52b39127fc8a760cfb2ea
Suresh-03 <sureshkumar732003@gmail.com>
2023-10-29 21:18:21 +0530

    98

---
+++ _98_validBinarySearchTree.java
@@ -0,0 +1,30 @@
+
+
+public class _98_validBinarySearchTree {
+ 
+ // Definition for a binary tree node.
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
+class Solution {
+    long min = Long.MIN_VALUE;
+    public boolean isValidBST(TreeNode root) {
+        if(root == null) return true;
+        if(!isValidBST(root.left)) return false;
+        if(min >= root.val) return false;
+        min = root.val;
+        if(!isValidBST(root.right)) return false;
+        return true;
+    }
+}
+}
