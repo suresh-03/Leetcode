2a89d897dabbbc9e3069257f3036a584aa5c8a24
Suresh-03 <sureshkumar7303@gmail.com>
2023-12-16 07:20:45 +0530

    242

---
+++ _242_validAnagram.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class _242_validAnagram {
+    class Solution {
+        public boolean isAnagram(String s, String t) {
+            char[] s1 = s.toCharArray();
+            char[] s2 = t.toCharArray();
+    
+            Arrays.sort(s1);
+            Arrays.sort(s2);
+    
+            if(Arrays.equals(s1,s2)){
+                return true;
+            }
+            return false;
+        }
+    }
+}
